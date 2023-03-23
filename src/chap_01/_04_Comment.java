a1a3ee732ce52597891160d1b473d649b125cbcf
jintae kim <vuenos@naver.com>
2023-03-23 18:12:43 +0900

    One line, Multie line Commment

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,20 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        // 주석
+        System.out.println("(10분 전) 잠시 후 행사가 시작됩니다.");
+//        System.out.println("(5분 전) 잠시 후 행사가 시작됩니다.");
+        System.out.println("지금부터 행사를 시작합니다.");
+
+        int size = 120;
+        size = size + 10; // 어린이는 발이 금방 자라서 사이즈 10이 더 큰걸로 구매
+
+        System.out.println("신발 사이즈 " + size + " 으로 살게요");
+
+        /*int a = 10;
+        int b = 20;
+        System.out.println(a + b);*/
+        // 여러줄 주석 ctrl + shift + /
+    }
+}
